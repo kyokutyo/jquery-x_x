96f452cf2373be2a4365913e91281eac7c9a0904
kyokutyo <haruta@kyokutyo.com>
2012-08-19 22:15:14 +0900

    defaults -> $.fn.x_x.defaults

--- jquery.x_x.js
+++ jquery.x_x.js
@@ -1,14 +1,6 @@
 ;(function($, undefined) {
     $.fn.x_x = function(options) {
-        var defaults = {
-            colors: [
-                '#f09', '#f03', '#600',
-                '#0f9', '#0f3', '#063',
-                '#09f', '#03f', '#036'
-            ],
-            interval: 50
-        };
-        var opts = $.extend(defaults, options);
+        var opts = $.extend({}, $.fn.x_x.defaults, options);
         return this.each(function() {
           var def_color, timer;
           $(this).on('mouseover', function() {
@@ -24,5 +16,13 @@
             $(this).css('color', def_color);
           });
         });
+    };
+    $.fn.x_x.defaults = {
+        colors: [
+            '#f09', '#f03', '#600',
+            '#0f9', '#0f3', '#063',
+            '#09f', '#03f', '#036'
+        ],
+        interval: 50
     };
 })(jQuery);
